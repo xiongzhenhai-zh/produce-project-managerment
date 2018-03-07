d8df830aec7f11ccfa78364da5890c4dbe17f144
xiongzhenhai <zhenhai.xiong@mi-me.com>
2018-03-07 13:49:17 +0800

    新增Spring Boot2.0框架

--- src/main/java/com/carejava/core/build/code/service/SpringBootService.java
+++ src/main/java/com/carejava/core/build/code/service/SpringBootService.java
@@ -4,7 +4,6 @@ import com.carejava.core.build.code.IBuild;
 import com.carejava.core.build.model.Model;
 import com.carejava.core.build.model.ModelAttribute;
 import com.carejava.core.build.model.Project;
-import com.carejava.core.build.utils.SystemUtil;
 import com.carejava.core.build.utils.Util;
 import org.beetl.core.Configuration;
 import org.beetl.core.GroupTemplate;
@@ -20,16 +19,15 @@ import java.io.IOException;
  * 〈一句话功能简述〉<br>
  * 〈功能详细描述〉
  *
- * @author zhenhai.xiong
- * @version [V1.0, 2017年10月24日]
+ * @author 熊振海    zhenhai.xiong@mi-me.com
+ * @version [V1.0,  2018年03月07日]
  */
-@Component("defaultServer")
-public class DefaultServer implements IBuild {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(DefaultServer.class);
+@Component("springBootService")
+public class SpringBootService implements IBuild {
+    private final static Logger LOGGER = LoggerFactory.getLogger(SpringBootService.class);
 
     private ClasspathResourceLoader resourceLoader = new ClasspathResourceLoader(
-            "template/server/mime");
+            "template/server/springboot");
 
     private Configuration cfg;
 
@@ -40,7 +38,7 @@ public class DefaultServer implements IBuild {
     }
 
     @Override
-    public void build(Project projectTemp, String rootPath) {
+    public void build(Project project, String rootPath) {
         try {
             cfg = Configuration.defaultConfiguration();
             gt = new GroupTemplate(resourceLoader, cfg);
@@ -48,66 +46,22 @@ public class DefaultServer implements IBuild {
             e.printStackTrace();
         }
 
-        initPom(rootPath, projectTemp);
+        initPom(rootPath, project);
 
         rootPath += "src/";
 
         // 初始构建资源文件
-        initResource(rootPath, projectTemp);
+        initResource(rootPath, project);
 
         // 初始化构建mapper
-        initMapper(rootPath + "main/resources", projectTemp);
+        initMapper(rootPath + "main/resources", project);
 
         // 初始化构建java文件
-        initServer(rootPath + "main/java", projectTemp);
+        initServer(rootPath + "main/java", project);
 
         // 初始化构建静态java文件
-        initStatic(rootPath + "main/java", projectTemp);
-
-        // 初始化构建web.xml文件
-        initWeb(rootPath + "main/webapp/WEB-INF/", projectTemp);
-    }
-
-    private void initStatic(String rootPath, Project project) {
-        String srcBasePath = rootPath + "/" + project.getPkgName().replace(".", "/");
-        String corePath = srcBasePath + "/core/";
-        Util.createFolders(corePath);
-        Template core = gt.getTemplate("/core.beetl");
-        core.binding("project", project);
-        Util.createFile(corePath, "HibernateAwareObjectMapper.java", core.render());
-
-        Template resultBean = gt.getTemplate("/resultBean.beetl");
-        resultBean.binding("project", project);
-        Util.createFile(corePath, "ResultBean.java", resultBean.render());
-
-        Template pageResultBean = gt.getTemplate("/pageResultBean.beetl");
-        pageResultBean.binding("project", project);
-        Util.createFile(corePath, "PageResultBean.java", pageResultBean.render());
-
-        String enumPath = srcBasePath + "/enums/";
-        Util.createFolders(enumPath);
-        Template enums = gt.getTemplate("/enums.beetl");
-        enums.binding("project", project);
-        Util.createFile(enumPath, "ErrorCode.java", enums.render());
-
-        String businessPath = srcBasePath + "/business/";
-        String businessImplPath = businessPath + "impl/";
-        Template baseBusiness = gt.getTemplate("/baseBusiness.beetl");
-        baseBusiness.binding("project", project);
-        Util.createFile(businessImplPath, "BaseBusiness.java", baseBusiness.render());
-
-        String baseControllerPath = srcBasePath + "/controller/";
-        Template baseController = gt.getTemplate("/baseController.beetl");
-        baseController.binding("project", project);
-        Util.createFile(baseControllerPath, "BaseController.java", baseController.render());
-    }
-
-    private void initWeb(String rootPath, Project project) {
-        Util.createFolders(rootPath);
-        // 创建application.xml
-        Template web = gt.getTemplate("/web.beetl");
-        web.binding("imerlgo", project);
-        Util.createFile(rootPath, "web.xml", web.render());
+        initStatic(rootPath + "main/java", project);
+        LOGGER.info("Spring Boot 构建完成");
     }
 
     private void initPom(String rootPath, Project project) {
@@ -121,38 +75,27 @@ public class DefaultServer implements IBuild {
     private void initResource(String rootPath, Project project) {
         String resourcePath = rootPath + "main/resources/";
         Util.createFolders(resourcePath);
-        // 创建servlet.xml
-        Template servlet = gt.getTemplate("/servlet.beetl");
-        servlet.binding("project", project);
-        Util.createFile(resourcePath, project.getArtifactId() + "-servlet.xml", servlet.render());
 
         // 创建sql
         Template sql = gt.getTemplate("/sql.beetl");
         sql.binding("imerlgo", project);
         Util.createFile(resourcePath, "createDB.sql", sql.render());
 
-        // 创建mybatis.xml
-        Template mybatis = gt.getTemplate("/mybatis.beetl");
-        mybatis.binding("project", project);
-        Util.createFile(resourcePath, "mybatis-config.xml", mybatis.render());
+        // 创建application.yml
+        Template application = gt.getTemplate("/application_yml.beetl");
+        application.binding("project", project);
+        Util.createFile(resourcePath, "application.yml", application.render());
+    }
 
-        String applicationRootPath = rootPath + "main/resources/application/";
-        Util.createFolders(applicationRootPath);
-        // 创建application.xml
-        Template applicationContext = gt.getTemplate("/applicationContext.beetl");
-        applicationContext.binding("project", project);
-        Util.createFile(applicationRootPath, "applicationContext.xml", applicationContext.render());
-
-        // 创建application-aopTx.xml
-        Template applicationAopTx = gt.getTemplate("/applicationContext-aopTx.beetl");
-        applicationAopTx.binding("project", project);
-        Util.createFile(applicationRootPath, "applicationContext-aopTx.xml", applicationAopTx.render());
-
-        // 创建application-dao.xml
-        Template applicationDao = gt.getTemplate("/applicationContext-dao.beetl");
-        applicationDao.binding("project", project);
-        Util.createFile(applicationRootPath, "applicationContext-dao.xml", applicationDao.render());
-
+    private void initMapper(String rootPath, Project project) {
+        String maperPath = rootPath + "/mapper/";
+        Util.createFolders(maperPath);
+        for (Model model : project.getModels()) {
+            Template mapper = gt.getTemplate("/mapper.beetl");
+            mapper.binding("model_xzh", model);
+            mapper.binding("project", project);
+            Util.createFile(maperPath, model.getName() + "Mapper.xml", mapper.render());
+        }
     }
 
     private void initServer(String rootPath, Project project) {
@@ -224,21 +167,48 @@ public class DefaultServer implements IBuild {
             Util.createFile(inVoPath, model.getName() + "VO.java", vo.render());
 
         }
-
     }
 
+    private void initStatic(String rootPath, Project project) {
+        String srcBasePath = rootPath + "/" + project.getPkgName().replace(".", "/");
+        String corePath = srcBasePath + "/core/";
+        Util.createFolders(corePath);
 
-    public void initMapper(String rootPath, Project project) {
-        String old1 = SystemUtil.getProjectRootPath() + "WEB-INF/classes/log4j.properties";
-        Util.copyFile(old1, rootPath + "/log4j.properties");
+        Template resultBean = gt.getTemplate("/resultBean.beetl");
+        resultBean.binding("project", project);
+        Util.createFile(corePath, "ResultBean.java", resultBean.render());
 
-        String maperPath = rootPath + "/mapper/";
-        Util.createFolders(maperPath);
-        for (Model model : project.getModels()) {
-            Template mapper = gt.getTemplate("/mapper.beetl");
-            mapper.binding("model_xzh", model);
-            mapper.binding("project", project);
-            Util.createFile(maperPath, model.getName() + "Mapper.xml", mapper.render());
-        }
+        Template pageResultBean = gt.getTemplate("/pageResultBean.beetl");
+        pageResultBean.binding("project", project);
+        Util.createFile(corePath, "PageResultBean.java", pageResultBean.render());
+
+        String enumPath = srcBasePath + "/enums/";
+        Util.createFolders(enumPath);
+        Template enums = gt.getTemplate("/enums.beetl");
+        enums.binding("project", project);
+        Util.createFile(enumPath, "ErrorCode.java", enums.render());
+
+        String businessPath = srcBasePath + "/business/";
+        String businessImplPath = businessPath + "impl/";
+        Template baseBusiness = gt.getTemplate("/baseBusiness.beetl");
+        baseBusiness.binding("project", project);
+        Util.createFile(businessImplPath, "BaseBusiness.java", baseBusiness.render());
+
+        String baseControllerPath = srcBasePath + "/controller/";
+        Template baseController = gt.getTemplate("/baseController.beetl");
+        baseController.binding("project", project);
+        Util.createFile(baseControllerPath, "BaseController.java", baseController.render());
+
+        // spring boot启动程序
+        String startUpPath = srcBasePath + "/";
+        Template startUp = gt.getTemplate("/startup.beetl");
+        startUp.binding("project", project);
+        Util.createFile(startUpPath, "SpringBootStartApplication.java", startUp.render());
+
+        //拦截器，用于访问静态资源文件
+        String staticPath = srcBasePath + "/";
+        Template staticResource = gt.getTemplate("/StaticResourceConfig.beetl");
+        staticResource.binding("project", project);
+        Util.createFile(staticPath, "StaticResourceConfig.java", staticResource.render());
     }
 }
